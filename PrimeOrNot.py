d5e025eb8d739153f56c510babe1c13bd93ee700
FaniA8 <narjesa147@gmail.com>
2021-12-15 20:37:29 +0330

    changed the PrimeOrNot code

--- PrimeOrNot.py
+++ PrimeOrNot.py
@@ -1,13 +1,15 @@
 number = int(input())
 
 res = True
-
-for i in range(2, number):
-	if number%i == 0:
-		res = False
+if (not number == 0) or (not number == 1):
+	for i in range(2, number):
+		if number%i == 0:
+			res = False
 
 if res:
 	print('The number is Prime.')
-elif number == 0 or number == 1 or res:
+elif (number == 0) or  (number == 1):
+	print('The number is not Prime.')
+else:
 	print('The number is not Prime.')
 
